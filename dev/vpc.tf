9a68e14651e422f236539e93e37bebc481f16ae0
Prajwal Akhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-12-16 12:15:22 +0530

    Create vpc.tf

---
+++ dev/vpc.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-2"
+   # source  = "hashicorp/aws"
+   # version = "3.74.0"
+    shared_credentials_file = "/home/ubuntu/.aws/credentials"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "prajwal-vpc"
+  cidr = "30.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b",]
+  private_subnets = ["30.0.1.0/24", "30.0.2.0/24",]
+  public_subnets  = ["30.0.101.0/24", "30.0.102.0/24",]
+
+  enable_nat_gateway = true
+#  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "test"
+  }
+}
